8c334f97c8ce881027d33be02e016e8f8bae7a42
AntonioBr98 <antonio.braccolino@outlook.it>
2022-02-13 11:25:43 +0100

    Initial Commit

---
+++ Pictopia/PictopiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictopiaApp.swift
+//  Pictopia
+//
+//  Created by Antonio Braccolino on 13/02/22.
+//
+
+import SwiftUI
+
+@main
+struct PictopiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
