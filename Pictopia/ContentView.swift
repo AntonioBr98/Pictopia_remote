edbd63716f2ad771bd2162caa11f66650d0f3cd4
AntonioBr98 <antonio.braccolino@outlook.it>
2022-02-13 12:03:53 +0100

    prova commit 2

--- Pictopia/ContentView.swift
+++ Pictopia/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Se riuscite a vedere questo messaggio, tutto appostos")
             .padding()
         //prova github
     }
