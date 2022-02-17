c88af5859fff712d68569cfc3232baec02f0a721
AntonioBr98 <antonio.braccolino@outlook.it>
2022-02-17 14:54:53 +0100

    Commit commentato

---
+++ Pictopia/CloudKit/CloudKitImages.swift
@@ -0,0 +1,101 @@
+//
+//  CloudKitImages.swift
+//  Pictopia
+//
+//  Created by Antonio Braccolino on 16/02/22.
+//
+
+
+import Foundation
+import UIKit
+import CoreData
+import CloudKit
+
+
+class Post {
+    
+    static let typeKey = "Post"
+    static let photoDataKey = "photoData"
+    static let timestampKey = "timestamp"
+    
+    init(photoData: Data?, timestamp: Date = Date(), comments: [Comment] = []) {
+        self.timestamp = timestamp
+        self.photoData = photoData
+        self.comments = comments
+    }
+    
+    convenience required init?(record: CKRecord) {
+        
+        guard let timestamp = record.creationDate,
+            let photoAsset = record[Post.photoDataKey] as? CKAsset else { return nil }
+        
+        let photoData = try? Data(contentsOf: photoAsset.fileURL)
+        self.init(photoData: photoData, timestamp: timestamp)
+        cloudKitRecordID = record.recordID
+    }
+    
+    var cloudKitRecord: CKRecord {
+        let recordID = cloudKitRecordID ?? CKRecordID(recordName: UUID().uuidString)
+        
+        let record = CKRecord(recordType: recordType, recordID: recordID)
+        
+        record[Post.timestampKey] = timestamp as CKRecordValue?
+        record[Post.photoDataKey] = CKAsset(fileURL: temporaryPhotoURL)
+        
+        return record
+    }
+    
+    
+    fileprivate var temporaryPhotoURL: URL {
+        
+        // Must write to temporary directory to be able to pass image file path url to CKAsset
+        
+        let temporaryDirectory = NSTemporaryDirectory()
+        let temporaryDirectoryURL = URL(fileURLWithPath: temporaryDirectory)
+        let fileURL = temporaryDirectoryURL.appendingPathComponent(UUID().uuidString).appendingPathExtension("jpg")
+        
+        try? photoData?.write(to: fileURL, options: [.atomic])
+        
+        return fileURL
+    }
+    
+    let timestamp: Date
+    let photoData: Data?
+    
+    var comments: [Comment] {
+        didSet {
+            DispatchQueue.main.async {
+                NotificationCenter.default.post(name: PostController.PostCommentsChangedNotification, object: self)
+            }
+        }
+    }
+    
+    var photo: UIImage? {
+        guard let photoData = self.photoData else { return nil }
+        return UIImage(data: photoData)
+    }
+    
+    var recordType: String { return Post.typeKey }
+    var cloudKitRecordID: CKRecordID?
+}
+
+
+extension Post: SearchableRecord {
+    func matches(searchTerm: String) -> Bool {
+        let matchingComments = comments.filter { $0.matches(searchTerm: searchTerm) }
+        return !matchingComments.isEmpty
+    }
+}
+
+
+//struct CloudKitImages: View {
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//}
+//
+//struct CloudKitImages_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CloudKitImages()
+//    }
+//}
--- Pictopia/CloudKit/CloudKitUser.swift
+++ Pictopia/CloudKit/CloudKitUser.swift
@@ -1,116 +1,116 @@
+////
+////  CloudKitUser.swift
+////  Pictopia
+////
+////  Created by Antonio Braccolino on 14/02/22.
+////
 //
-//  CloudKitUser.swift
-//  Pictopia
+//import SwiftUI
+//import CloudKit
 //
-//  Created by Antonio Braccolino on 14/02/22.
 //
-
-import SwiftUI
-import CloudKit
-
-
-class CloudKitUserViewModel: ObservableObject{
-    
-    @Published var perimissionStatus: Bool = false
-    @Published var isSignedInToiCloud: Bool = false
-    @Published var error: String = ""
-    @Published var userName: String = ""
-    
-    init (){
-        getiCloudStatus()
-        requestPermission()
-        fetchiCloudUserRecodID()
-    }
-    
-//    UTENTE HA BISOGNO DI ICLOUD ACCOUNT PER ACCEDERE
-    private func getiCloudStatus() {
-//        WEAK REFERENCES PERCHè USO CODICE ASINCRONO
-        CKContainer.default().accountStatus { [weak self] returnedStatus, returnedError in
-            DispatchQueue.main.async {
-                switch returnedStatus {
-                    
-                case .couldNotDetermine:
-                    self?.error = CloudKitError.iCloudAccountNotDetermined.rawValue
-                    
-                case .available:
-                    self?.isSignedInToiCloud = true
-                    
-                case .restricted:
-                    self?.error = CloudKitError.iCloudAccountRestricted.rawValue
-                    
-                case .noAccount:
-                    self?.error = CloudKitError.iCloudAccountNotFound.rawValue
-                    
-                default:
-                    self?.error = CloudKitError.iCloudAccountUnknown.rawValue
-                }
-            }
-        }
-    }
-    enum CloudKitError: String, LocalizedError {
-        case iCloudAccountNotFound
-        case iCloudAccountNotDetermined
-        case iCloudAccountRestricted
-        case iCloudAccountUnknown
-    }
-    
-    func requestPermission() {
-        CKContainer.default().requestApplicationPermission([.userDiscoverability]) { [weak self] returnedStatus, returnedError in
-            DispatchQueue.main.async {
-                if returnedStatus == .granted {
-                    self?.perimissionStatus = true
-                }
-                
-               
-                
-        }
-    }
-    }
-    
-    
-    
-    func fetchiCloudUserRecodID(){
-        CKContainer.default().fetchUserRecordID { [weak self] returnedID, returnedError in
-            if let id = returnedID {
-                self?.discoveriCloudUser(id: id)
-            }
-        }
-    }
-    
-    
-    
-    
-    func discoveriCloudUser(id: CKRecord.ID){
-        CKContainer.default().discoverUserIdentity(withUserRecordID: id) { [weak self] returnedIdentity, returnedError in
-            DispatchQueue.main.async {
-                if let name = returnedIdentity?.nameComponents?.givenName{
-                    self?.userName = name
-                }
-            }
-        }
-    }
-    
-}
-
-struct CloudKitUser: View {
-    
-    @StateObject private var vm = CloudKitUserViewModel()
-    
-    var body: some View {
-        
-        VStack{
-            
-            Text("IS SIGNED IN: \(vm.isSignedInToiCloud.description.uppercased())")
-            Text(vm.error)
-            Text("Permission: \(vm.perimissionStatus.description.uppercased())")
-            Text("NAME: \(vm.userName)")
-        }
-       
-    }
-}
-
-struct CloudKitUser_Previews: PreviewProvider {
-    static var previews: some View {
-        CloudKitUser()
-    }
-}
+//class CloudKitUserViewModel: ObservableObject{
+//    
+//    @Published var perimissionStatus: Bool = false
+//    @Published var isSignedInToiCloud: Bool = false
+//    @Published var error: String = ""
+//    @Published var userName: String = ""
+//    
+//    init (){
+//        getiCloudStatus()
+//        requestPermission()
+//        fetchiCloudUserRecodID()
+//    }
+//    
+////    UTENTE HA BISOGNO DI ICLOUD ACCOUNT PER ACCEDERE
+//    private func getiCloudStatus() {
+////        WEAK REFERENCES PERCHè USO CODICE ASINCRONO
+//        CKContainer.default().accountStatus { [weak self] returnedStatus, returnedError in
+//            DispatchQueue.main.async {
+//                switch returnedStatus {
+//                    
+//                case .couldNotDetermine:
+//                    self?.error = CloudKitError.iCloudAccountNotDetermined.rawValue
+//                    
+//                case .available:
+//                    self?.isSignedInToiCloud = true
+//                    
+//                case .restricted:
+//                    self?.error = CloudKitError.iCloudAccountRestricted.rawValue
+//                    
+//                case .noAccount:
+//                    self?.error = CloudKitError.iCloudAccountNotFound.rawValue
+//                    
+//                default:
+//                    self?.error = CloudKitError.iCloudAccountUnknown.rawValue
+//                }
+//            }
+//        }
+//    }
+//    enum CloudKitError: String, LocalizedError {
+//        case iCloudAccountNotFound
+//        case iCloudAccountNotDetermined
+//        case iCloudAccountRestricted
+//        case iCloudAccountUnknown
+//    }
+//    
+//    func requestPermission() {
+//        CKContainer.default().requestApplicationPermission([.userDiscoverability]) { [weak self] returnedStatus, returnedError in
+//            DispatchQueue.main.async {
+//                if returnedStatus == .granted {
+//                    self?.perimissionStatus = true
+//                }
+//                
+//               
+//                
+//        }
+//    }
+//    }
+//    
+//    
+//    
+//    func fetchiCloudUserRecodID(){
+//        CKContainer.default().fetchUserRecordID { [weak self] returnedID, returnedError in
+//            if let id = returnedID {
+//                self?.discoveriCloudUser(id: id)
+//            }
+//        }
+//    }
+//    
+//    
+//    
+//    
+//    func discoveriCloudUser(id: CKRecord.ID){
+//        CKContainer.default().discoverUserIdentity(withUserRecordID: id) { [weak self] returnedIdentity, returnedError in
+//            DispatchQueue.main.async {
+//                if let name = returnedIdentity?.nameComponents?.givenName{
+//                    self?.userName = name
+//                }
+//            }
+//        }
+//    }
+//    
+//}
+//
+//struct CloudKitUser: View {
+//    
+//    @StateObject private var vm = CloudKitUserViewModel()
+//    
+//    var body: some View {
+//        
+//        VStack{
+//            
+//            Text("IS SIGNED IN: \(vm.isSignedInToiCloud.description.uppercased())")
+//            Text(vm.error)
+//            Text("Permission: \(vm.perimissionStatus.description.uppercased())")
+//            Text("NAME: \(vm.userName)")
+//        }
+//       
+//    }
+//}
+//
+//struct CloudKitUser_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CloudKitUser()
+//    }
+//}
